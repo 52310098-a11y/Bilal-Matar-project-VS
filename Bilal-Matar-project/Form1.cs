a8f0ef655d86ddf742750c8f118f39212d726d42
52310098-a11y <52310098@students.liu.edu.lb>
2026-01-04 20:18:04 +0200

    Add project files.

---
+++ Bilal-Matar-project/Form1.cs
@@ -0,0 +1,15 @@
+namespace Bilal_Matar_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
